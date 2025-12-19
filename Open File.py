a46f0b26205f808c6e1b31a706f377e4d0f3f268
Benaboura Ibrahim <ibrahimbenaboura6@gmail.com>
2025-12-19 18:08:08 +0100

    first commit

---
+++ Open File.py
@@ -0,0 +1,40 @@
+TheFile = open("==> THE FILE PATH HERE <==", "r")
+TheTries = 3
+ThePassword = "==> YOUR PASSWORD HERE"
+FileContent = TheFile.read()
+
+
+while TheTries > 0 :
+    userpassword = input("Put The Password : ")
+
+
+
+    if userpassword == ThePassword :
+        print(FileContent)
+        break
+
+
+
+    else : 
+        TheTries -= 1
+        print("The Password Is Wrong.")
+        print(f"You Have {TheTries} Tries Left.")
+
+
+
+else :
+    print("Your Tries Are Done.")
+
+TheFile.close()      
+
+
+
+
+
+
+
+
+
+
+
+
